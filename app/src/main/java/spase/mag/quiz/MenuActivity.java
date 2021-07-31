63afa756dce948cd888b1845f40f9389b2f56740
cris1972 <cris72@mail.ru>
2021-07-31 16:04:37 +0300

    work button return in menu

--- app/src/main/java/spase/mag/quiz/MenuActivity.java
+++ app/src/main/java/spase/mag/quiz/MenuActivity.java
@@ -2,7 +2,10 @@ package spase.mag.quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        Button btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
